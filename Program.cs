5931c1003877e6381c59bf336556190c0dfa5d91
stanislavshabalinn <shabalin_sa1@mail.ru>
2024-10-22 23:42:34 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Moduie10_z1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Hello, World!");
+
+                Console.Write("Введите число а: ");
+
+                int a = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Введите число b: ");
+
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                Summa summa = new Summa();
+
+                int j = summa.Summ(a, b);
+
+                Console.Write("Сумма а + b = " + j);
+            }
+            catch
+            {
+                Console.Write("Сумму а + b посчитать невозможно, так как они не являются целыми числами!");
+            }
+
+        }
+
+        public interface ISumm
+        {
+            int Summ(int a, int b);
+        }
+
+        public class Summa : ISumm
+        {
+            public int Summ(int a, int b)
+            {
+                return a + b;
+            }
+        }
+    }
+}
